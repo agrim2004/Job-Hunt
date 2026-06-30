11a03b1912dcaee5dac0374f97b09e97bbab2885
Agrim <agrimvij442@gmail.com>
2026-06-30 15:33:03 +0530

    Update CORS for Vercel deployment

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:['http://localhost:5173', "http://localhost:5174"],
+    origin:["http://localhost:5173", 
+        "http://localhost:5174",
+        process.env.FRONTEND_URL],
     credentials:true
 }
 
