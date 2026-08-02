05e5f41792902a91c42c8de9000dcc62031b7969
Agrim Vij <160526180+agrim2004@users.noreply.github.com>
2026-08-02 16:59:57 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -121,8 +121,8 @@ export const login = async (req, res) => {
             .cookie("token", token, {
                 maxAge: 1 * 24 * 60 * 60 * 1000,
                 httpOnly: true,
-                sameSite: "strict",
-                secure: process.env.NODE_ENV === "production"
+                sameSite: "none",
+                secure: true
             })
             .json({
                 message: `Welcome back ${user.fullname}`,
