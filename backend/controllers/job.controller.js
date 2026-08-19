7a14ff6394022db6a09c9b9f6c62c51033f79b05
Agrim Vij <160526180+agrim2004@users.noreply.github.com>
2026-08-19 15:18:30 +0530

    Update job.controller.js

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -64,16 +64,23 @@ export const getAllJobs = async (req, res) => {
 export const getJobById = async (req, res) => {
     try {
         const jobId = req.params.id;
-       const job = await Job.findById(jobId)
-    .populate("company")
-    .populate("applications");
+
+        const job = await Job.findById(jobId)
+            .populate("company")
+            .populate("applications");
+
         if (!job) {
             return res.status(404).json({
                 message: "Jobs not found.",
                 success: false
-            })
-        };
-        return res.status(200).json({ job, success: true });
+            });
+        }
+
+        return res.status(200).json({
+            job,
+            success: true
+        });
+
     } catch (error) {
         console.log(error);
     }
